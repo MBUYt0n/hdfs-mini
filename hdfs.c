eab43a8b75f574bef405f3f3e00e3a75f9d904b4
MBUYt0n <shusrith03@gmail.com>
2024-01-16 05:08:38 +0530

    addfile

--- hdfs.c
+++ hdfs.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#define MAX_BLOCKS 100
+#define MAX_DATA_NODES 10
 
 typedef struct FileMetadata
 {
@@ -27,3 +33,38 @@ typedef struct DataNodeMetadata
     int storageAvailable;
 } dnm;
 
+typedef struct NameNode
+{
+    fm files[MAX_BLOCKS];
+    bm blocks[MAX_BLOCKS];
+    dnm data_nodes[MAX_DATA_NODES];
+    int num_files;
+    int num_blocks;
+    int num_data_nodes;
+} NameNode;
+
+typedef struct DataNode
+{
+    int node_id;
+    char data[1024];
+} DataNode;
+
+void initialiseNamenode(NameNode *nn)
+{
+    nn->num_blocks = 0;
+    nn->num_data_nodes = 0;
+    nn->num_files = 0;
+}
+
+void addFile(NameNode *nn, char *filename, char *path)
+{
+    fm newFile;
+    newFile.created = time(NULL);
+    newFile.modified = time(NULL);
+    strcpy(newFile.filename, filename);
+    strcpy(newFile.path, path);
+    struct stat file_info;
+    int size = lstat(filename, &file_info);
+    newFile.size = size;
+    nn->files[nn->num_files++] = newFile;
+}
--- x.c
+++ x.c
@@ -1,32 +1,56 @@
-#include <stdlib.h>
+// #include <stdlib.h>
+// #include <stdio.h>
+// #include <unistd.h>
+// #include <sys/types.h>
+// #include <sys/stat.h>
+// #include <fcntl.h>
+
+// int main()
+// {
+//     FILE *fp = fopen("tt.txt", "r");
+
+//     if (fp == NULL)
+//     {
+//         perror("Error opening file");
+//         return 1;
+//     }
+
+//     int file_descriptor = fileno(fp);
+//     struct stat file_info;
+
+//     if (fstat(file_descriptor, &file_info) == -1)
+//     {
+//         perror("Error getting file information");
+//         fclose(fp);
+//         return 1;
+//     }
+
+//     printf("File size: %ld bytes\n", file_info.st_size);
+
+//     fclose(fp);
+//     return 0;
+// }
+
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
+#include <unistd.h>
 
 int main()
 {
-    FILE *fp = fopen("tt.txt", "r");
-
-    if (fp == NULL)
-    {
-        perror("Error opening file");
-        return 1;
-    }
-
-    int file_descriptor = fileno(fp);
+    const char *filename = "example.txt";
     struct stat file_info;
 
-    if (fstat(file_descriptor, &file_info) == -1)
+    if (lstat(filename, &file_info) == -1)
     {
         perror("Error getting file information");
-        fclose(fp);
         return 1;
     }
 
     printf("File size: %ld bytes\n", file_info.st_size);
 
-    fclose(fp);
+    // You can access other file information using the members of struct stat
+
     return 0;
 }
